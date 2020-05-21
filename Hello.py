8205c685bfbf14040b4ba2db2b2a35ba662f4bc6
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-21 15:08:16 +0800

    variable ok

--- Hello.py
+++ Hello.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+@app.route('/hello/<name>')
+def hello_world(name):
+    return 'Hello world %s !' % name
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
