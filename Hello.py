67aeee5ff3f268917fe481f2f12a90ef949940ca
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-21 19:26:00 +0800

    added revision

--- Hello.py
+++ Hello.py
@@ -1,9 +1,16 @@
+#converters
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/hello/<name>')
-def hello_world(name):
-    return 'Hello world %s !' % name
+@app.route('/blog/<int:postID>')
+def hello_world(postID):
+    return 'Hello world %d !' % postID
+
+@app.route('/rev/<float:revNO>')
+def revision(revNO):
+    return 'Revision number %f' % revNO
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 80)
+
+
