40b224ea197d85fc9be76c31f1eb900d02d1e6b1
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-23 11:00:24 +0800

    a f()

---
+++ Hello.py
@@ -0,0 +1,16 @@
+#converters
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/blog/<int:postID>')
+def hello_world(postID):
+    return 'Hello world %d !' % postID
+
+@app.route('/rev/<float:revNO>')
+def revision(revNO):
+    return 'Revision number %f' % revNO
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 80)
+
+
